c9f846aa038012d65d1efb80fdd69abf6c7a90ea
yang <13307130285@fudan.edu.cn>
2018-12-14 07:32:40 +0800

    增加一个漫画网站

--- eimg_reader.py
+++ eimg_reader.py
@@ -16,6 +16,7 @@ url = 'https://m.k886.net/look/name/SuperDick/cid/36391/id/339212/p/2'
 url = 'https://manhwahand.com/manhwa/desire-king/chapter-30?style=list'
 # url = 'https://nhentai.net/g/249922/2/'
 # url = 'https://mangapark.me/manga/love-parameter-kkun/s1/c104/2'
+url = 'https://www.taadd.com/chapter/HMate59/798722-21.html'
 
 conf_loader = ConfigLoader()
 
