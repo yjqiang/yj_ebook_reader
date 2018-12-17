f8a82fba510ba6ebc30c503d408f337a6a6f8610
yang <13307130285@fudan.edu.cn>
2018-12-17 12:08:14 +0800

    timeout

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ def get(url, headers=None):
     url = url.strip()
     while True:
         try:
-            rsp = session.get(url, headers=headers, timeout=2.5)
+            rsp = session.get(url, headers=headers, timeout=4)
             if rsp.status_code != 200:
                 print(rsp.status_code, url)
             else:
