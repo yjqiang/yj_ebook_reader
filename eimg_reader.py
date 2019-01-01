67f66fb9f18a11d555ee0fbf88433271b452532a
yang <13307130285@fudan.edu.cn>
2019-01-01 09:01:08 +0800

    add
    我是不是该弄点正常18-的网站了……

--- eimg_reader.py
+++ eimg_reader.py
@@ -10,9 +10,11 @@ url = 'https://e-hentai.org/s/9962198a63/1300988-2'
 # url = 'https://nhentai.net/g/249922/2/'
 # url = 'https://mangapark.me/manga/love-parameter-kkun/s1/c104/2'
 # url = 'https://www.taadd.com/chapter/HMate59/798722-21.html'
+# url = 'https://original-work.simply-hentai.com/%E5%AE%B6%E6%97%8F%E3%81%AE%E3%81%9F%E3%82%81%E3%81%AB-%E5%89%8D%E7%B7%A8/page/6360412'
 # url = 'https://manganelo.com/chapter/hcampus/chapter_55'
 # url = 'https://www.wnacg.org/photos-view-id-4901546.html'
-        
+# url = 'https://manhwahentai.com/manhwa/the-sharehouse/chapter-31/?style=list'
+     
         
 controller = Controller(EImgLoader, EImgReader)
 
