142f9c91c57e6af0ba95655d471cfdf4898889d9
yang <13307130285@fudan.edu.cn>
2019-01-10 17:50:02 +0800

    调整

--- web.py
+++ web.py
@@ -3,6 +3,8 @@ import requests
 
 session = requests.Session()
 caches = {}
+
+
 def get(url, headers=None, allow_cache=True):
     url = url.strip()
     if url in caches:
@@ -10,13 +12,13 @@ def get(url, headers=None, allow_cache=True):
         return caches[url]
     while True:
         try:
-            rsp = session.get(url, headers=headers, timeout=4)
-            if rsp.status_code != 200:
-                print(rsp.status_code, url)
-            else:
-                if allow_cache:
-                    caches[url] = rsp
-                return rsp
+            with session.get(url, headers=headers, timeout=4) as rsp:
+                if rsp.status_code != 200 and rsp.status_code != 500:
+                    pass
+                else:
+                    if allow_cache:
+                        caches[url] = rsp
+                    return rsp
         except Exception as e:
             print(e)
             pass
